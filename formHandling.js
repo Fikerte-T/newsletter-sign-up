f558874cd7aea59a2c202c70d8f171e7ad0fbc4d
fikerte <tofikertesfaye@gmail.com>
2024-11-06 16:34:09 +0300

    Handle form submission

--- formHandling.js
+++ formHandling.js
@@ -1,9 +1,13 @@
+const formPage = document.getElementById('form-page')
+const successPage = document.getElementById('success-page')
+const emailInput = document.getElementById('email')
+
 const handleSubmit = (e) => {
   e.preventDefault()
   const formData = Object.fromEntries(new FormData(e.target))
   const emailData = formData.email.trim()
   const errorMessage = document.getElementById('errorMessage')
-  const emailInput = document.getElementById('email')
+  const submittedEmail = document.querySelector('.submitted-email')
     //validation
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
   if(!emailRegex.test(emailData)) {
@@ -14,6 +18,14 @@ const handleSubmit = (e) => {
     errorMessage.classList.add('hidden')
     errorMessage.classList.remove('block')
     emailInput.classList.remove('error')
-    // alert("success")
+    formPage.style.display = 'none'
+    successPage.style.display = "flex"
+    submittedEmail.innerHTML = emailData
   }
+}
+
+  const handleDismiss = () => {
+    successPage.style.display = 'none'
+    formPage.style.display = 'flex'
+    emailInput.value = ''
 }
